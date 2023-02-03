80ca0e82b52e517c9810751eb949db72985a8dfa
pnhung177 <pnhung177@gmail.com>
2023-02-04 04:15:56 +0700

    Enhances the npm scripts; Renames the field name profile.js/devebot to profile.js/framework

--- test/bdd/app-server-test.js
+++ test/bdd/app-server-test.js
@@ -2,32 +2,27 @@
 
 const Devebot = require("@saola/core");
 const chores = Devebot.require("chores");
-const envcloak = require("envcloak").instance;
 
 const app = require("../app");
 
 describe("@saola/plugin-restfetch", function() {
   describe("start/stop app.server", function() {
     before(function() {
-      envcloak.setup({
+      chores.setEnvironments({
         SAOLA_FORCING_SILENT: "framework",
         LOGOLITE_FULL_LOG_MODE: "false",
         LOGOLITE_ALWAYS_ENABLED: "all",
         LOGOLITE_ALWAYS_MUTED: "all"
       });
-      chores.clearCache();
     });
     //
     after(function() {
-      envcloak.reset();
       chores.clearCache();
     });
     //
-    it("app.server should be started/stopped properly", function(done) {
-      app.server.start().then(function() {
+    it("app.server should be started/stopped properly", function() {
+      return app.server.start().then(function() {
         return app.server.stop();
-      }).then(function() {
-        done();
       });
     });
   });
