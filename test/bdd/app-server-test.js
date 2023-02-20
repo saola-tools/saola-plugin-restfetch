ac12a412b8fd354883284954c088470df49dcf4f
pnhung177 <pnhung177@gmail.com>
2023-02-20 16:15:34 +0700

    Refactor the app-server-test

--- test/bdd/app-server-test.js
+++ test/bdd/app-server-test.js
@@ -6,20 +6,20 @@ const chores = Devebot.require("chores");
 const app = require("../app");
 
 describe("@saola/plugin-restfetch", function() {
+  before(function() {
+    chores.setEnvironments({
+      SAOLA_FORCING_SILENT: "framework",
+      LOGOLITE_FULL_LOG_MODE: "false",
+      LOGOLITE_ALWAYS_ENABLED: "all",
+      LOGOLITE_ALWAYS_MUTED: "all"
+    });
+  });
+  //
+  after(function() {
+    chores.clearCache();
+  });
+  //
   describe("start/stop app.server", function() {
-    before(function() {
-      chores.setEnvironments({
-        SAOLA_FORCING_SILENT: "framework",
-        LOGOLITE_FULL_LOG_MODE: "false",
-        LOGOLITE_ALWAYS_ENABLED: "all",
-        LOGOLITE_ALWAYS_MUTED: "all"
-      });
-    });
-    //
-    after(function() {
-      chores.clearCache();
-    });
-    //
     it("app.server should be started/stopped properly", function() {
       return app.server.start().then(function() {
         return app.server.stop();
